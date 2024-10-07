7f022724e8ed094557dcefeab73cf25685dba6ea
FJMonteroInformatica <fj.montero.informatica@gmail.com>
2024-10-07 12:54:47 +0200

    fix: Modify Windows initial_data

--- Windows/initial_data.ps1
+++ Windows/initial_data.ps1
@@ -6,11 +6,10 @@ Write-Host "_______________________INITIAL DATA_______________________"
 # Disable SSL verification
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-
 # Define JSON payload for creating a new user
 $jsonBodySignUp = @{
     username = "$username"
-    password = "$password"
+    password = "$passwordPlainText"
     authority = "ADMIN"
     email = "$email"
 } | ConvertTo-Json
@@ -21,7 +20,7 @@ Invoke-WebRequest -Uri "http://localhost:3001/api/user/signUp" -Method POST -Con
 # Sign in to get the access token
 $jsonBodySignIn = @{
     username = "$username"
-    password = "$password"
+    password = "$passwordPlainText"
 } | ConvertTo-Json
 
 $response = Invoke-WebRequest -Uri "http://localhost:3001/api/user/signIn" -Method POST -ContentType "application/json" -Body $jsonBodySignIn -UseBasicParsing
